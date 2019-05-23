0e69756abe8a38ca94d5624c02bf696923ad6d2d
Karl Stenerud <kstenerud@gmail.com>
2019-05-23 17:49:25 +0200

    More fiddling with ideas for an API

--- streamux.go
+++ streamux.go
@@ -1,43 +1,189 @@
 package streamux
 
-type codecState int
+type ProtocolState int
 
 const (
-	codecStateUninitialized = iota
-	codecStateSentInitializeMessage
-	codecStateInitialized
-	codecStateFailed
+	ProtocolStateUninitialized = iota
+	ProtocolStateSentInitializeMessage
+	ProtocolStateInitialized
+	ProtocolStateFailed
 )
 
-type Codec struct {
-	requestQuickInit bool
-	allowQuickInit   bool
-	lengthMinBits    int
-	lengthMaxBits    int
-	idMinBits        int
-	idMaxBits        int
-	lengthBits       int
-	idBits           int
-	state            codecState
+func calculateHeaderLength(idBits, lengthBits int) int {
+	totalBits := idBits + lengthBits
+	switch {
+	case totalBits <= 6:
+		return 1
+	case totalBits <= 14:
+		return 2
+	case totalBits <= 22:
+		return 3
+	}
+	return 4
+}
+
+type SendableMessage struct {
+	protocol           *Protocol
+	id                 uint32
+	idBits             int
+	lengthBits         int
+	headerLength       int
+	maxChunkLength     int
+	responseBitShifted uint32
+	lengthBitShift     uint
+	data               []byte
+	closed             bool
+}
+
+func (this *SendableMessage) Close() {
+	if this.closed {
+		return
+	}
+	this.protocol.freeId(this.id)
+	this.closed = true
+}
+
+func (this *SendableMessage) dataLength() int {
+	return len(this.data) - this.headerLength
+}
+
+func (this *SendableMessage) sendCurrentChunk(terminationBit uint32) {
+	length := uint32(this.dataLength())
+	headerFields := terminationBit |
+		this.responseBitShifted |
+		this.id<<idBitShift |
+		length<<this.lengthBitShift
+
+	for i := 0; i < this.headerLength; i++ {
+		this.data[i] = byte(headerFields)
+		headerFields >>= 8
+	}
+
+	this.protocol.sendMessage(this.data)
+}
+
+const (
+	responseBitShift = 1
+	idBitShift       = 2
+)
+
+func boolToUint32(value bool) uint32 {
+	if value {
+		return 1
+	}
+	return 0
+}
+
+func (this *SendableMessage) AddData(data []byte) {
+	for len(data) > 0 {
+		currentLength := this.dataLength()
+		lengthToAdd := len(data)
+		if currentLength+lengthToAdd > this.maxChunkLength {
+			lengthToAdd = this.maxChunkLength - currentLength
+		}
+		this.data = append(this.data, data[:lengthToAdd]...)
+		currentLength = this.dataLength()
+		if currentLength == this.maxChunkLength {
+			this.sendCurrentChunk(0)
+			data = data[lengthToAdd:]
+		}
+	}
+}
+
+func (this *SendableMessage) Complete() {
+	this.sendCurrentChunk(1)
+	this.Close()
+}
+
+func newSendableMessage(protocol *Protocol, id int, idBits int, lengthBits int, isResponse bool) *SendableMessage {
+	message := new(SendableMessage)
+	message.protocol = protocol
+	message.id = uint32(id)
+	message.idBits = idBits
+	message.lengthBits = lengthBits
+	message.headerLength = calculateHeaderLength(idBits, lengthBits)
+	message.maxChunkLength = 1<<uint(lengthBits) - 1
+	message.data = make([]byte, message.headerLength, message.maxChunkLength)
+	message.responseBitShifted = boolToUint32(isResponse) << responseBitShift
+	message.lengthBitShift = uint(idBitShift + idBits)
+
+	return message
+}
+
+type ProtocolCallbacks interface {
+	OnMessageChunkReceived(messageId int, isEnd bool, data []byte)
+	OnDataReadyToSend(data []byte)
+}
+
+type Protocol struct {
+	expectingInitMessage bool
+	requestQuickInit     int
+	allowQuickInit       int
+	lengthMinBits        int
+	lengthMaxBits        int
+	idMinBits            int
+	idMaxBits            int
+	lengthBits           int
+	idBits               int
+	state                ProtocolState
+	callbacks            ProtocolCallbacks
 }
 
 const protocolVersion = 1
 
-func NewCodec(lengthMinBits int, lengthMaxBits int, lengthRecommendBits int,
-	idMinBits int, idMaxBits int, idRecommendBits int) *Codec {
-	this := new(Codec)
+const (
+	shiftQuickInitRequest      = 30
+	shiftQuickInitAllowed      = 29
+	shiftLengthBitsMin         = 24
+	shiftLengthBitsMax         = 19
+	shiftLengthBitsRecommended = 14
+	shiftIdBitsMin             = 10
+	shiftIdBitsMax             = 5
+)
+
+const (
+	maskMin         = 0x0f
+	maskMax         = 0x1f
+	maskRecommended = 0x1f
+)
+
+func (this *Protocol) sendMessage(message []byte) {
+	this.callbacks.OnDataReadyToSend(message)
+}
+
+func (this *Protocol) freeId(id uint32) {
+	// TODO
+}
+
+func NewProtocol(lengthMinBits int, lengthMaxBits int, lengthRecommendBits int,
+	idMinBits int, idMaxBits int, idRecommendBits int, callbacks ProtocolCallbacks) *Protocol {
+	this := new(Protocol)
 	this.lengthMinBits = lengthMinBits
 	this.lengthMaxBits = lengthMaxBits
 	this.lengthBits = lengthRecommendBits
 	this.idMinBits = idMinBits
 	this.idMaxBits = idMaxBits
 	this.idMaxBits = idRecommendBits
-	// TODO: Quick Init
+	this.callbacks = callbacks
+
+	this.requestQuickInit = 0 // TODO
+	this.allowQuickInit = 0   // TODO
+
+	this.sendMessage(this.buildInitializeMessage())
+
 	return this
 }
 
-func (this *Codec) buildInitializeMessage() []byte {
-	var requestPieces uint32 = 0 // TODO
+func (this *Protocol) buildInitializeMessage() []byte {
+	requestPieces := this.idBits |
+		this.idMaxBits<<shiftIdBitsMax |
+		this.idMinBits<<shiftIdBitsMin |
+		this.lengthBits<<shiftLengthBitsRecommended |
+		this.lengthMaxBits<<shiftLengthBitsMax |
+		this.lengthMinBits<<shiftLengthBitsMin |
+		this.requestQuickInit<<shiftQuickInitRequest |
+		this.allowQuickInit<<shiftQuickInitAllowed
+
 	request := []byte{
 		protocolVersion,
 		byte(requestPieces >> 24),
@@ -47,18 +193,20 @@ func (this *Codec) buildInitializeMessage() []byte {
 	return request
 }
 
-func (this *Codec) NextMessage() []byte {
-	// TODO: block or not?
-	switch this.state {
-	case codecStateUninitialized:
-		this.state = codecStateSentInitializeMessage
-		return this.buildInitializeMessage()
-	default:
-		// TODO
-		return nil
-	}
+func (this *Protocol) nextFreeID() uint32 {
+	return 0
 }
 
-func (this *Codec) onInitiatorRequest(request []byte) {
+func (this *Protocol) BeginMessage() *SendableMessage {
+	// TODO
+	return nil
+}
+
+func (this *Protocol) BegiReplyMessage(responseToId int) *SendableMessage {
+	// TODO
+	return nil
+}
+
+func (this *Protocol) Feed(data []byte) {
 	// TODO
 }
