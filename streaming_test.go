560445b7a8f89ab2440ec96db9a4053f7a2ec594
Karl Stenerud <kstenerud@gmail.com>
2019-05-28 14:01:36 +0200

    More extreme end tests

--- streaming_test.go
+++ streaming_test.go
@@ -74,10 +74,13 @@ func TestStream(t *testing.T) {
 	for i := 20; i >= 2; i-- {
 		assertStreamData(t, i, 2, 99999)
 	}
+	for i := 10; i >= 1; i-- {
+		assertStreamData(t, i, 0, 10)
+	}
 }
 
 func TestEmpty(t *testing.T) {
-	for i := 29; i >= 2; i-- {
+	for i := 30; i >= 1; i-- {
 		assertStreamData(t, i, 0, 0)
 	}
 }
